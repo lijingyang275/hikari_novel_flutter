11fce7b703470ab396167f7b15d42ddb72090b3e
15dd <69246156+15dd@users.noreply.github.com>
2026-04-09 13:36:43 +0800

    欢迎页面加入节点选择 节点默认值改为.cc

--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hikari_novel_flutter/router/route_path.dart';
+import 'package:hikari_novel_flutter/service/local_storage_service.dart';
 import 'package:hikari_novel_flutter/widgets/state_page.dart';
+import '../../models/common/wenku8_node.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -19,7 +21,25 @@ class WelcomePage extends StatelessWidget {
             const SizedBox(height: 4),
             Text("welcome_tip".tr, style: TextStyle(fontSize: 14)),
             const SizedBox(height: 20),
-            TextButton.icon(onPressed: () => Get.toNamed(RoutePath.login), label: Text("go_to_login".tr), icon: const Icon(Icons.login))
+            FilledButton.icon(onPressed: () => Get.toNamed(RoutePath.login), label: Text("go_to_login".tr), icon: const Icon(Icons.login)),
+            const SizedBox(height: 40),
+            PopupMenuButton<Wenku8Node>(
+              onSelected: (Wenku8Node value) {
+                LocalStorageService.instance.setWenku8Node(value);
+              },
+              itemBuilder: (BuildContext context) => [
+                PopupMenuItem<Wenku8Node>(value: Wenku8Node.wwwWenku8Net, child: Text(Wenku8Node.wwwWenku8Net.node)),
+                PopupMenuItem<Wenku8Node>(value: Wenku8Node.wwwWenku8Cc, child: Text(Wenku8Node.wwwWenku8Cc.node)),
+              ],
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.lan_outlined, size: 16, color: Theme.of(context).colorScheme.primary),
+                  SizedBox(width: 8),
+                  Text("node".tr, style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+                ],
+              ),
+            ),
           ],
         ),
       ),
